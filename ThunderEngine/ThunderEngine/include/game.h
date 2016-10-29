1ac130c8d9a16b0a0a85733127642642e0118dd0
MightyChubz <alexc@occultvisions.com>
2016-10-29 03:03:49 -0400

    FIXED YAY!

--- ThunderEngine/ThunderEngine/include/game.h
+++ ThunderEngine/ThunderEngine/include/game.h
@@ -3,22 +3,15 @@
 #ifndef game_h__
 #define game_h__
 
-#ifndef THUNDERENGINE_EXPORTS
-#define THUNDERENGINE_API __declspec(dllimport);
+#ifdef THUNDERENGINE_EXPORTS
+#define THUNDERENGINE_API __declspec(dllexport)
 #else
-#define THUNDERENGINE_API __declspec(dllexport);
-#endif // THUNDERENGINE_EXPORTS
+#define THUNDERENGINE_API __declspec(dllimport)
+#endif
 
 namespace ThunderEngine
 {
-	struct THUNDERENGINE_API WindowProperties
-	{
-		const char* title;
-		int width;
-		int height;
-		int x;
-		int y;
-	};
+	
 }
 
 #endif // game_h__
--- ThunderEngine/ThunderEngine/source/game.cpp
+++ ThunderEngine/ThunderEngine/source/game.cpp
@@ -1,1 +1,12 @@
 #include "../include/game.h"
+
+
+
+game::game()
+{
+}
+
+
+game::~game()
+{
+}
